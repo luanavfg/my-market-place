4e2a0af1903c37de11370073a84b63c8db6b2c17
Luana <luanavefago1@gmail.com>
2023-06-23 09:34:28 -0300

    feat: manage extensions

--- gqlClient.ts
+++ gqlClient.ts
@@ -4,6 +4,8 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+// import { persistCache } from 'apollo-cache-persist';
+// import localforage from 'localforage';
 
 export const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_FAUNA_DOMAIN,
@@ -25,10 +27,35 @@ export const setAuthToken = (token: string) => setContext((_,
       ...headers,
       authorization: `Bearer ${token}`,
     }
-  }))
+}))
 
+const isServer = typeof window === 'undefined';
 export const client = new ApolloClient({
-  // ssrMode: typeof window === 'undefined',
+  // ssrMode: isServer,
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
+// export const createApolloClient = async () => {
+//   const isServer = typeof window === 'undefined';
+
+//   const cache = new InMemoryCache();
+
+//   // Persist the cache
+//   if (!isServer) {
+//     await persistCache({
+//       cache,
+//       storage: localforage as any, // Cast localforage to any to avoid TypeScript error
+//     });
+//   }
+
+//   const client = new ApolloClient({
+//     ssrMode: isServer,
+//     link: authLink.concat(httpLink),
+//     cache,
+//   });
+
+//   return client;
+// };
+
+// export const client = createApolloClient();
